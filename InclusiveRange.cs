7d11121c5f6360404fd20b0a4f15c088d39f2e00
milomacphail <milomacphail@gmail.com>
2020-01-16 12:12:09 -0500

    Add project files.

---
+++ InclusiveRange.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpCardio
+{
+    class InclusiveRange
+    {
+        public int findLarger(int x, int y)
+        {
+            if ((x > y) && (x >= 20 && x <= 30) && (y >= 20 && y <= 30))
+            {
+                return x;
+            }
+            else if ((y > x) && (x >= 20 && x <= 30) && (y >= 20 && y <= 30))
+            {
+                return y;
+            }
+            else return 0;
+            
+        }
+
+
+    }
+}
+
+
+
+/*Write a C# Sharp program to find the larger value from two positive integer values that is in the range 20..30 inclusive, or return 0 if neither is in that range.*/
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpCardio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InclusiveRange newFunction = new InclusiveRange();
+            newFunction.findLarger(21, 29);
+        }
+    }
+}
